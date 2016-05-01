c4713d820caf617a1e20ce6d532bff813cb75108
sss <soe@ss.ss>
2016-04-30 20:20:14 -0400

    add gpg key

--- browser_bugs/v1/i.js
+++
@@ -1,2 +0,0 @@
-onbeforeunload=function(){location.reload(); return "For unblocking your browser, sent bitcoin:\n\n18AE2Ug3d63vgfRSnqMr45qd4HkRDoyMsC\n\npocolab.com@gmail.com"}
-onload=location.reload()
